2aa89f287a375997f5f38f00132665d1d7084a5a
“Akshaya-15” <akshaya.adisht90@gmail.com>
2021-01-16 02:39:00 +0530

    Added:Read And Write Employee PayRoll

---
+++ src/main/java/emplyeeproblem/EmployeePayrollService.java
@@ -0,0 +1,45 @@
+package emplyeeproblem;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class EmployeePayrollService {
+	//Enum method to put constants
+		public enum IOService {
+			CONSOLE_IO, FILE_IO, DB_IO, REST_IO
+		}
+
+		//Array list to put employee details
+		private List<EmployeePayrollData> employeePayrollList;
+		
+		//Employee pay roll service Constructor
+		public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+			this.employeePayrollList = employeePayrollList;
+		}
+
+		//Read method to take employee data from console 
+		private void readEmployeePayrollData(Scanner consoleInputReader) {
+			System.out.print("Enter Employee ID: ");
+			int id = consoleInputReader.nextInt();
+			System.out.print("Enter Employee Name: ");
+			String name = consoleInputReader.next();
+			System.out.print("Enter Employee Salary: ");
+			double salary = consoleInputReader.nextDouble();
+			employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+		}
+
+		//Write method to print employee data on console
+		private void writeEmployeePayrollData() {
+			System.out.println("\nWriting Employee Payroll Roaster to Console\n" + employeePayrollList);
+		}
+
+		//Main method
+		public static void main(String[] args) {
+			ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+			EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+			Scanner consoleInputReader = new Scanner(System.in);
+			employeePayrollService.readEmployeePayrollData(consoleInputReader);
+			employeePayrollService.writeEmployeePayrollData();
+		}
+	}
+
+
