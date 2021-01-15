49a94f01034eb232dc77e1458b44769c5aaeb6ab
“Akshaya-15” <akshaya.adisht90@gmail.com>
2021-01-16 02:49:41 +0530

    Added:Demostrate File Opration

---
+++ src/main/java/emplyeeproblem/FileUtils.java
@@ -0,0 +1,14 @@
+package emplyeeproblem;
+import java.io.File;
+public class FileUtils {
+	//Delete Method
+		public static boolean deleteFiles(File contentsToDelete) {
+			File[] allContents = contentsToDelete.listFiles();
+			if (allContents != null) {
+				for (File file : allContents)
+					deleteFiles(file);
+			}
+			return contentsToDelete.delete();
+		}
+	}
+
